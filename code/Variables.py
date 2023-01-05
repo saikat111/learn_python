9bfaa504c4ae2e3c8395ea1500b881f3e0cb33eb
saikat111 <s8856010@gmail.com>
2023-01-06 03:17:48 +0600

    Variables

---
+++ code/Variables.py
@@ -0,0 +1,15 @@
+x = 10
+#print(x)
+name = 'saikat'
+#print(name)
+
+#Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+#Get the Type
+print(type(z))
+
+ 
