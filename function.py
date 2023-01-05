d8f193ad1351374f0fd8d0a41dc44adb8a881485
saikat111 <s8856010@gmail.com>
2023-01-06 04:02:33 +0600

    function

--- function.py
+++ function.py
@@ -11,3 +11,11 @@ def my_function(*kids):
   print("The youngest child is " + kids[0])
 
 my_function("Emil", "Tobias", "Linus")
+
+
+#Arbitrary Keyword Arguments, **kwargs
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
