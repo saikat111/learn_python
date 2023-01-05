2200998ef2bee59b9aa5af39c485c0966f82b718
saikat111 <s8856010@gmail.com>
2023-01-06 03:56:42 +0600

    if else

---
+++ code/If ... Else.py
@@ -0,0 +1,15 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+  #Short Hand If
+  if a > b: print("a is greater than b")
+
+  #Short Hand If ... Else
+  a = 2
+  b = 330
+  print("A") if a > b else print("B")
