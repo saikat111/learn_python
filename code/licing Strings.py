82b25f5ddd82dea6a8506769b83fb85450f0d5f4
saikat111 <s8856010@gmail.com>
2023-01-06 03:39:43 +0600

    Slicing Strings

---
+++ code/licing Strings.py
@@ -0,0 +1,15 @@
+b = "Hello, World!"
+print(b[0:3])
+
+#Slice From the Start
+b = "Hello, World!"
+print(b[:5])
+
+#Slice To the End
+b = "Hello, World!"
+print(b[2:])
+
+
+#Negative Indexing
+b = "Hello, World!"
+print(b[-5:-2])
