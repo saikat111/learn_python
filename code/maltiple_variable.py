23814bed6d90244c8819ec68b68c3ba7117bfa1b
saikat111 <s8856010@gmail.com>
2023-01-06 03:22:58 +0600

    Assign Multiple Values

---
+++ code/maltiple_variable.py
@@ -0,0 +1,20 @@
+#Many Values to Multiple Variables
+x, y, z,name = "Orange", "Banana", "Cherry","saikat"
+print(x)
+print(y)
+print(z)
+print(name)
+
+
+#One Value to Multiple Variables
+x = y = z = 100
+print(z)
+
+
+#Unpack a Collection
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
