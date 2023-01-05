788809a10c3f911a389352fb306f20bb6f1784d5
saikat111 <s8856010@gmail.com>
2023-01-06 03:19:20 +0600

    Variables

--- code/Variables.py
+++ code/Variables.py
@@ -12,4 +12,13 @@ z = float(3)  # z will be 3.0
 #Get the Type
 print(type(z))
 
+
+#vartiable can be define as
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
  
